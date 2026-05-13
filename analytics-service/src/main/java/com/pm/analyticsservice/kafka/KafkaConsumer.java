5efd3fb3a4c7107eff2bccb24573f93d3f09452e
PhilimonElkessDawoud <P.E.DAOUD@student.aast.edu>
2026-05-13 15:48:56 +0300

    Debugging analytics-service with kafka

--- analytics-service/src/main/java/com/pm/analyticsservice/kafka/KafkaConsumer.java
+++ analytics-service/src/main/java/com/pm/analyticsservice/kafka/KafkaConsumer.java
@@ -1,6 +1,8 @@
 package com.pm.analyticsservice.kafka;
 
 import com.google.protobuf.InvalidProtocolBufferException;
+import jakarta.annotation.PostConstruct;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -11,9 +13,26 @@ import patient.events.PatientEvent;
 public class KafkaConsumer {
     private static final Logger log = LoggerFactory.getLogger(KafkaConsumer.class);
 
+    @PostConstruct
+    public void init() {
+        log.info("🔥 Kafka consumer bean initialized");
+    }
+
     @KafkaListener(topics = "patient", groupId = "analytics-service")
-    public void consumeEvent(byte[] event) {
+    public void listen(byte[] message) {
+        log.info("🔥 RECEIVED MESSAGE: {}", new String(message));
+    }
+
+    @KafkaListener(topics = "patient", groupId = "analytics-service-2")
+    public void consumeEvent(ConsumerRecord<String, byte[]> record) {
+
+        log.info("🔥 RECEIVED MESSAGE");
+
+        byte[] event = record.value();
+
         try {
+            log.info("Received raw event of size: {}", event.length);
+
             PatientEvent patientEvent = PatientEvent.parseFrom(event);
             //Perform analytics business logic here.
 
