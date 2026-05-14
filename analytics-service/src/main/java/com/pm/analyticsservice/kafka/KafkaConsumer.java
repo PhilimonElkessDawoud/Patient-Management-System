6502586d83a3127065dd82da887d29fb5d48983c
PhilimonElkessDawoud <P.E.DAOUD@student.aast.edu>
2026-05-14 21:42:28 +0200

    Kafka debugged & Api gateway phase 1 done

--- analytics-service/src/main/java/com/pm/analyticsservice/kafka/KafkaConsumer.java
+++ analytics-service/src/main/java/com/pm/analyticsservice/kafka/KafkaConsumer.java
@@ -1,8 +1,6 @@
 package com.pm.analyticsservice.kafka;
 
 import com.google.protobuf.InvalidProtocolBufferException;
-import jakarta.annotation.PostConstruct;
-import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -11,39 +9,22 @@ import patient.events.PatientEvent;
 
 @Service
 public class KafkaConsumer {
-    private static final Logger log = LoggerFactory.getLogger(KafkaConsumer.class);
 
-    @PostConstruct
-    public void init() {
-        log.info("🔥 Kafka consumer bean initialized");
-    }
+    private static final Logger log = LoggerFactory.getLogger(
+            KafkaConsumer.class);
 
-    @KafkaListener(topics = "patient", groupId = "analytics-service")
-    public void listen(byte[] message) {
-        log.info("🔥 RECEIVED MESSAGE: {}", new String(message));
-    }
-
-    @KafkaListener(topics = "patient", groupId = "analytics-service-2")
-    public void consumeEvent(ConsumerRecord<String, byte[]> record) {
-
-        log.info("🔥 RECEIVED MESSAGE");
-
-        byte[] event = record.value();
-
+    @KafkaListener(topics="patient", groupId = "analytics-service")
+    public void consumeEvent(byte[] event) {
         try {
-            log.info("Received raw event of size: {}", event.length);
-
             PatientEvent patientEvent = PatientEvent.parseFrom(event);
-            //Perform analytics business logic here.
+            // ... perform any business related to analytics here
 
-            log.info("Received Patient Event: [PatientId={}, PatientName={}, PatientEmail={} ]",
+            log.info("Received Patient Event: [PatientId={},PatientName={},PatientEmail={}]",
                     patientEvent.getPatientId(),
                     patientEvent.getName(),
                     patientEvent.getEmail());
-
         } catch (InvalidProtocolBufferException e) {
             log.error("Error deserializing event {}", e.getMessage());
         }
-
     }
 }
